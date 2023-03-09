29e210fcdfab36d28c5e7aa09eef8cb01eb625a3
tianxu <aoxiaotian@foxmail.com>
2023-03-09 17:48:17 +0800

    add Lottery NFT & Second Creation NFT (incomplete)

---
+++ RemixProjects/CustomNFTContracts/LotteryNFTWithRoyalty.sol
@@ -0,0 +1,24 @@
+// contracts/MyNFT.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MyNFT is ERC721 {
+    constructor() ERC721("MyNFT", "MNFT") {
+    }
+     /**
+   * generates a pseudorandom number
+   * @param seed a value ensure different outcomes for different sources in the same block
+   * @return a pseudorandom value
+   */
+  function random(uint256 seed) public view returns (uint256) {
+    return uint256(keccak256(abi.encodePacked(
+      tx.origin,
+      blockhash(block.number - 1),
+      block.timestamp,
+      seed
+    )));
+  }
+
+}
